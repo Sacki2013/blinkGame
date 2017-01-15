815be9ace3b2bf35b328c0b5266e06ae9e6d438e
Adam Sackfield <Sacki2013@gmail.com>
2017-01-15 03:27:03 +0000

    Added readme. Also tweaked core on game. Also high score writes to file

--- blink.js
+++ blink.js
@@ -1,6 +1,8 @@
 // Require JohnnyFive and Create a board
 var five = require("johnny-five");
 var board = new five.Board();
+// Used for storing/pulling the high score.
+var fs = require('fs');
 
 // Initialize variables
 var counter = 0; // Used to restet on last Led and return to start
@@ -8,6 +10,30 @@ var score = 0;
 var gameOver = false; // Triggers results when true
 var level = 600; // Starting speed
 var leds = [];
+// Read current high score from fil
+var highScore = fs.readFileSync('highscore.txt');
+
+// Arrays for emoji faces.
+var happyFace = [
+  '00000000',
+  '01100110',
+  '00000000',
+  '00011000',
+  '00000000',
+  '01000010',
+  '00111100',
+  '00000000'
+];
+var sadFace = [
+  '00000000',
+  '01100110',
+  '00000000',
+  '00011000',
+  '00000000',
+  '00111100',
+  '01000010',
+  '00000000'
+];
 
 board.on("ready", function() {
   // Initialize Components
@@ -36,11 +62,13 @@ board.on("ready", function() {
   // Listening for the button to be pressed. It will then check if the value of counter is equal to the green
   // led(target). If it is then it will reduce the level value, therefore increasing the speed, and makes a call
   // again to the startGame function. This time when the level is entered it is faster. This will continue
-  // until you lose, at which point counter is reset and the scoreLed~() function is started.
+  // until you lose, at which point counter is reset and the scoreLed() function is started. Also the score is
+  // output to the matrix as we go.
   button.on("press", function() {
     clearInterval(ledLoop);
     if(counter == 3) {
       score++;
+      ledMatrix.on().draw(score);
       if(score < 5) {
         level -= 75;
       } else if(score < 11) {
@@ -70,14 +98,21 @@ board.on("ready", function() {
     }, level);
   }
 
+  // Random function. Not in use but perhaps a second button could trigger random game
   function randBtn() {
     var rand = Math.floor((Math.random() * 5));
     return rand;
   }
-  // The score function used the draw function to display the score to the matrix after 3 seconds it will turn off the display. After a further 1 second it will exit the program. Lightout is called at the start to shut down the leds.
+
+  // Compares high score to score and if you beat it you get a smiley, if not then sad face. Board waits are used to shut down the ledMatric and exit the program.
   function scoreLed(score) {
+    compareHigh(score, highScore);
     lightsOut();
-    ledMatrix.on().draw(score);
+    if(score > highScore) {
+      ledMatrix.on().draw(happyFace);
+    } else {
+      ledMatrix.on().draw(sadFace);
+    }
     board.wait(3000,function(){
       ledMatrix.off();
       board.wait(1000, function(){
@@ -87,9 +122,19 @@ board.on("ready", function() {
   }
 
   // Lights will loop leds and turn them off
-  function lightsOut(){
+  function lightsOut() {
     for(var i = 0; i < leds.length; i++) {
         leds[i].off();
+    }
+  }
+
+  // Function to compare highScore. If it is beaten then it will be written to file.
+  function compareHigh(score, highScore) {
+    if(score > highScore) {
+      fs.writeFile('highscore.txt', score);
+      return true;
+    } else {
+      return false;
     }
   }
 });
---
+++ readme.md
@@ -0,0 +1,25 @@
+# Blink Game - Arduino/Node project
+## Created by Adam Sackfield
+
+### Parts
+* Arduino Uno
+* 5 Leds (4 x Red, 1 x Green)
+* 6 Reistors (5 x 220ohm, 1 x 1000ohm)
+* 8 x 8 Led Matrix (MAX7219)
+* Breadboard
+* Button
+* Jumper cables
+
+### Wiring
+* Leds
+  ** Top to bottom. Anode to Aruidno pins 13 > 9
+  ** Resistors (220) Cathode to ground
+* Button(Switch)
+  ** Pin 8 on Arduino. Resistor to ground
+  ** Other leg to mains
+* Matrix
+  ** Clock to Pin 5
+  ** CS to Pin 6
+  ** Data to Pin 7
+
+  
